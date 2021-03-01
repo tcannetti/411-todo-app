89406fa4b6c1df2e824ba47be8d8e91350303221
tcannetti <tcannetti@gmail.com>
2021-03-01 14:29:10 -0600

    bunch of changes and functionality added up to task class change on double click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Doc Appt',
+        day: "Feb 5th",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: "Feb 12th",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Appt',
+        day: "Feb 20th",
+        reminder: true,
+      },
+    ]
+  )
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: 
+          !task.reminder } : task
+    )
+  )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : ('No Tasks to Show'
+      )}    
     </div>
   );
 }
